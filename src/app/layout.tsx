e3676b1dcf89e7f64f307c22d14411ad6c14fbff
Paul <paul@effl.com>
2026-07-04 06:33:56 +0000

    feat: Yandex Webmaster verification tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  verification: {
+    yandex: "8a33d2220d56bd48",
+  },
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return children;
 }
